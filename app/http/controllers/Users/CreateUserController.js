7b989d8be4ad44eeac6a57173e7b0a8612df0524
Goegrow <augusto.pires@goegrow.com.br>
2024-12-13 16:27:58 -0300

    Adiciona Controllers e Rotas e Models do schema.prisma

---
+++ app/http/controllers/Users/CreateUserController.js
@@ -0,0 +1,9 @@
+const Prisma = require('../../database/index')
+
+module.exports = {
+
+    async create(req, res){
+
+    }
+
+}
--- app/http/routes/routes.js
+++ app/http/routes/routes.js
@@ -3,6 +3,10 @@ const Router = express.Router()
 
 Router.get('/', (req, res) => {
     res.render('index')
-  })
+})
+
+Router.get('/dashboard', (req, res) => {
+    res.render('dashboard')
+})
 
  module.exports = Router
